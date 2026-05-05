f08e420ef886a507e0361bbee79e38a497dcb145
Jesus Moreno <jesusmoreno9418@csu.fullerton.edu>
2026-05-04 21:43:35 -0700

    Magnet test file working

--- scripts/electromagnetic.py
+++ scripts/electromagnetic.py
@@ -1,1 +1,20 @@
+import serial
+import time
 
+PORT = "COM3"
+BAUD_RATE = 9600
+
+arduino = serial.Serial(PORT, BAUD_RATE, timeout=1)
+time.sleep(2)
+
+print("Turning magnet ON")
+arduino.write(b"MAGNET ON\n")
+print("Arduino:", arduino.readline().decode().strip())
+
+time.sleep(8)
+
+print("Turning magnet OFF")
+arduino.write(b"MAGNET OFF\n")
+print("Arduino:", arduino.readline().decode().strip())
+
+arduino.close()
